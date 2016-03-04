faad97419a719af49c8b4bb388b3c97ab9ea9a5d
Victor Semionov <vsemionov@gmail.com>
2016-03-04 06:36:08 +0200

    removed comments

--- hippo/api/urls.py
+++ hippo/api/urls.py
@@ -5,12 +5,9 @@ from . import views
 
 
 router = routers.DefaultRouter()
-# register job endpoint in the router
 router.register(r'jobs', views.JobViewSet)
 router.register(r'users', views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
