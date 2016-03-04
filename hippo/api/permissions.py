2ee468e28e3824ccb96e6abc3ef96f17bd779ebe
Victor Semionov <vsemionov@gmail.com>
2016-03-04 19:01:10 +0200

    filter user jobs and allow superuser access to everything

--- hippo/api/permissions.py
+++ hippo/api/permissions.py
@@ -1,6 +1,30 @@
+from django.db.models import Q
 from rest_framework import permissions
+
+from .models import Job
 
 
 class JobPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner or request.method in permissions.SAFE_METHODS and obj.public
+        return request.user.is_superuser or request.user == obj.owner or request.method in permissions.SAFE_METHODS and obj.public
+
+def get_jobs_filter_arg(request):
+    q = Q(public=True)
+    if request.user.is_authenticated():
+        q |= Q(owner=request.user)
+    return q
+
+def get_jobs_filter(request):
+    if request.user.is_superuser:
+        return Job.objects.all()
+    q = get_jobs_filter_arg(request)
+    return Job.objects.filter(q)
+
+def get_user_jobs_filter_func(request):
+    def user_jobs_filter_func(instance):
+        job_set = instance.job_set
+        if request.user.is_superuser:
+            return job_set.all()
+        q = get_jobs_filter_arg(request)
+        return instance.job_set.filter(q)
+    return user_jobs_filter_func
--- hippo/api/views.py
+++ hippo/api/views.py
@@ -1,10 +1,9 @@
-from django.db.models import Q
 from django.contrib.auth.models import User
-from rest_framework import mixins, viewsets, permissions
+from rest_framework import mixins, viewsets, permissions, serializers
 
 from .models import Job
 from .serializers import JobSerializer, UserSerializer
-from .permissions import JobPermissions
+from .permissions import JobPermissions, get_jobs_filter, get_user_jobs_filter_func
 
 
 class JobViewSet(mixins.CreateModelMixin,
@@ -17,15 +16,23 @@ class JobViewSet(mixins.CreateModelMixin,
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, JobPermissions)
 
     def get_queryset(self):
-        q = Q(public=True)
-        if self.request.user.is_authenticated():
-            q |= Q(owner=self.request.user)
-        return Job.objects.filter(q)
+        return get_jobs_filter(self.request)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class UserViewSet(mixins.ListModelMixin,
+
+# ugly hack to filter jobs related to users
+# failed: https://github.com/tomchristie/django-rest-framework/issues/1985
+# failed: https://github.com/tomchristie/django-rest-framework/issues/1935
+class SecuredFieldMixin(object):
+    def get_serializer(self, *args, **kwargs):
+        serializer = super(SecuredFieldMixin, self).get_serializer(*args, **kwargs)
+        userser = serializer if not isinstance(serializer, serializers.ListSerializer) else serializer.child
+        userser.fields['jobs'].get_attribute = get_user_jobs_filter_func(self.request)
+        return serializer
+
+class UserViewSet(SecuredFieldMixin, mixins.ListModelMixin,
                   mixins.RetrieveModelMixin,
                   viewsets.GenericViewSet):
     queryset = User.objects.all()
