1173d78001875a8fedacc4e43f2a4f9adf753bd3
Victor Semionov <vsemionov@gmail.com>
2016-03-05 07:48:10 +0200

    Revert "refactoring"
    This reverts commit d9ddc888406920071d41ac7f8265ce1a5a42306e.

--- hippo/api/models.py
+++ hippo/api/models.py
@@ -12,10 +12,8 @@ class Job(models.Model):
     ))
 
     owner = models.ForeignKey('auth.User', editable=False)
-    state = models.CharField(choices=STATES.items(), default=STATES['pending'], max_length=10, editable=False)
-
     public = models.BooleanField(default=False)
-    notify = models.BooleanField(default=False)
+    state = models.CharField(choices=STATES.items(), default=STATES['pending'], max_length=10, editable=False)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
