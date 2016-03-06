c8d84cf4b197404aec0dbe6ee7d34fc146830ae4
Victor Semionov <vsemionov@gmail.com>
2016-03-06 23:02:39 +0200

    simplified configuration

--- hippo/proxy.sh
+++ hippo/proxy.sh
@@ -4,6 +4,6 @@ set -e
 
 DIR=`dirname $0`
 
-sed -e "s/\\\$WEB_HOST/$WEB_HOST/g" -e "s/\\\$WORKER_CONNECTIONS/$WORKER_CONNECTIONS/g" $DIR/nginx.conf >/tmp/nginx.conf
+sed -e "s/\\\$WEB_HOST/$WEB_HOST/g" $DIR/nginx.conf >/tmp/nginx.conf
 rm /var/log/nginx/error.log && ln -s /dev/null /var/log/nginx/error.log # workaround: prevent the nginx build on this image, which logs to a hard-coded location, from growing its log indefinitely
 nginx -c /tmp/nginx.conf
--- hippo/worker.sh
+++ hippo/worker.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-su -m hippo -c "celery -A hippo.celery worker --concurrency \$CONCURRENCY --loglevel INFO"
+export LOCAL_SETTINGS_FILE=/hippo/hippo/hippo/worker.conf
+su -m hippo -c "celery -A hippo.celery worker --concurrency \$CONCURRENCY --maxtasksperchild \$MAX_TASKS_PER_CHILD --loglevel INFO"
