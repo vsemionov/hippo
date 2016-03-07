1fb91c6a22c680641b0b16f0226e5f1a350e2185
Victor Semionov <vsemionov@gmail.com>
2016-03-07 22:34:08 +0200

    retry on connection errors

--- hippo/api/tasks.py
+++ hippo/api/tasks.py
@@ -1,7 +1,10 @@
 from functools import wraps
+import socket
 
+from django.db import OperationalError
 from django.conf import settings
 from django.core.mail import send_mail
+
 from celery import shared_task
 
 from .models import Job
@@ -26,13 +29,24 @@ def process_job(fn):
                 notify.delay(job.owner.email, job_url)
     return wrapper
 
-@shared_task
+def retry_job(fn):
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except (socket.error, OperationalError) as exc:
+            self.retry(exc=exc, countdown=30, max_retries=2**31)
+    return wrapper
+
+@shared_task(bind=True)
+@retry_job
 def notify(owner_email, job_url):
     subject = 'Hippo job completed'
     message = 'Your Hippo job has been completed. You can view the results here: {url}'.format(url=job_url)
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, (owner_email,))
 
-@shared_task
+@shared_task(bind=True)
+@retry_job
 @process_job
 def power(n):
     return 2 ** n
