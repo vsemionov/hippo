ede80e789f558d7bf19313b0bc2a901144fd681f
Victor Semionov <vsemionov@gmail.com>
2016-03-09 01:11:23 +0200

    no input content validation

--- hippo/api/models.py
+++ hippo/api/models.py
@@ -12,9 +12,6 @@ def file_size_validator(value):
     if value.size > settings.MAX_UPLOAD_FILE_SIZE:
         raise ValidationError("Maximum file size exceeded")
 
-def file_content_validator(value):
-    pass
-
 class Job(models.Model):
     STATES = OrderedDict((
         ('pending', 'pending'),
@@ -32,7 +29,7 @@ class Job(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    input = models.FileField(db_index=True, upload_to=user_dir, validators=[file_size_validator, file_content_validator])
+    input = models.FileField(db_index=True, upload_to=user_dir, validators=[file_size_validator])
     result = models.FileField(null=True, db_index=True, editable=False, upload_to=user_dir)
 
     result_id = models.CharField(null=True, max_length=36, editable=False)
