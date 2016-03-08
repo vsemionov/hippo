b1ff214ac12227265d18f250db84300dc9a0df98
Victor Semionov <vsemionov@gmail.com>
2016-03-08 16:19:47 +0200

    file content validator placeholder

--- hippo/api/models.py
+++ hippo/api/models.py
@@ -12,6 +12,9 @@ def file_size_validator(value):
     if value.size > settings.MAX_UPLOAD_FILE_SIZE:
         raise ValidationError("Maximum file size exceeded")
 
+def file_content_validator(value):
+    pass
+
 class Job(models.Model):
     STATES = OrderedDict((
         ('pending', 'pending'),
@@ -29,5 +32,5 @@ class Job(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    input = models.FileField(upload_to=user_dir, validators=[file_size_validator])
+    input = models.FileField(upload_to=user_dir, validators=[file_size_validator, file_content_validator])
     result_id = models.CharField(null=True, max_length=36, editable=False)
