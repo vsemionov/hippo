ad71d54c44128cb5c95ad01c834cdfa879066dd5
Victor Semionov <vsemionov@gmail.com>
2016-03-10 00:56:57 +0200

    reordered fields

--- hippo/api/models.py
+++ hippo/api/models.py
@@ -22,15 +22,15 @@ class Job(models.Model):
     ))
 
     owner = models.ForeignKey('auth.User', editable=False)
+    async_id = models.CharField(null=True, max_length=36, editable=False)
 
     public = models.BooleanField(default=False)
     notify = models.BooleanField(default=False)
 
+    state = models.CharField(choices=STATES.items(), default=STATES['pending'], max_length=10, editable=False)
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-    async_id = models.CharField(null=True, max_length=36, editable=False)
-    state = models.CharField(choices=STATES.items(), default=STATES['pending'], max_length=10, editable=False)
 
     input = models.FileField(db_index=True, upload_to=user_dir, validators=[file_size_validator])
     output = models.FileField(null=True, db_index=True, editable=False, upload_to=user_dir)
