a031d6f9e82b2000e2c7a17b428bdd46809cb0aa
Victor Semionov <vsemionov@gmail.com>
2016-03-10 07:53:58 +0200

    make development environment

---
+++ bin/mkdevenv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+DIR=`dirname $0`
+
+cd $DIR/..
+cp docker-compose.dev.yml docker-compose.override.yml
+cd hippo
+openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=Hippo'
+python manage.py collectstatic <<EOF
+yes
+EOF
