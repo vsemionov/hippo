783bf3fe2625273588f697176f32898103b7dbbc
Victor Semionov <vsemionov@gmail.com>
2016-03-11 10:06:42 +0200

    poll and validate job state, validate stored input

--- bin/test.sh
+++ bin/test.sh
@@ -8,22 +8,22 @@ PROXY_IP=`docker inspect --format '{{ .NetworkSettings.Networks.hippo_default.IP
 MQ_IP=`docker inspect --format '{{ .NetworkSettings.Networks.hippo_default.IPAddress }}' hippo_mq_1`
 
 
-echo "waiting for the web service..."
+echo "waiting for the mq..."
+until (echo > /dev/tcp/$MQ_IP/5672) &>/dev/null
+do
+	echo "waiting..."
+sleep 2
+done
+
+echo "waiting for the web..."
 until (echo > /dev/tcp/$WEB_IP/8000) &>/dev/null
 do
 	echo "waiting..."
 sleep 2
 done
 
-echo "waiting for the proxy service..."
+echo "waiting for the proxy..."
 until (echo > /dev/tcp/$PROXY_IP/443) &>/dev/null
-do
-	echo "waiting..."
-sleep 2
-done
-
-echo "waiting for the mq service..."
-until (echo > /dev/tcp/$MQ_IP/5672) &>/dev/null
 do
 	echo "waiting..."
 sleep 2
@@ -31,18 +31,42 @@ done
 
 
 TEMPDIR=`mktemp -d`
-echo "graphs = True" >$TEMPDIR/test.npc
+TEMPFILE=`mktemp`
+INPUT=$TEMPDIR/test.npc
+echo "graphs = True" >$INPUT
 
 echo "posting test job..."
-JOB_URL=`http -a admin:admin --verify=no --form POST https://$PROXY_IP/jobs/ input@$TEMPDIR/test.npc public=true | jq -r '.url'`
+JOB_URL=`http -a admin:admin --verify=no --form POST https://$PROXY_IP/jobs/ input@$INPUT public=true | jq -r '.url'`
+echo $JOB_URL
 [ $JOB_URL == "null" ] && exit 1
 
-echo "waiting for output and results..."
-http --headers --follow --verify=no HEAD $JOB_URL | head -n 1 | fgrep "200 OK"
-OUTPUT_URL=null
-while [ $OUTPUT_URL == "null" ]; do echo "waiting for output..."; sleep 1; OUTPUT_URL=`http --follow --verify=no $JOB_URL | jq -r '.output'`; done
-RESULTS_URL=null
-while [ $RESULTS_URL == "null" ]; do echo "waiting for results..."; sleep 1; RESULTS_URL=`http --follow --verify=no $JOB_URL | jq -r '.output'`; done
+echo "retrieving input url..."
+INPUT_URL=`http --follow --verify=no $JOB_URL | jq -r '.input'`
+echo $INPUT_URL
+[ $INPUT_URL == "null" ] && exit 1
+
+echo "checking stored input..."
+http --follow --verify=no $INPUT_URL >$TEMPFILE
+diff $TEMPFILE $INPUT || exit 1
+
+echo "waiting for job completion..."
+JOB_STATE="pending"
+while [ $JOB_STATE == "pending" ] || [ $JOB_STATE == "started" ];
+do
+	echo "waiting for job completion"
+	sleep 1
+	JOB_STATE=`http --follow --verify=no $JOB_URL | jq -r '.state'`
+done
+echo $JOB_STATE
+[ $JOB_STATE != "finished" ] && exit 1
+
+echo "retrieving output and results urls..."
+OUTPUT_URL=`http --follow --verify=no $JOB_URL | jq -r '.output'`
+echo $OUTPUT_URL
+[ $OUTPUT_URL == "null" ] && exit 1
+RESULTS_URL=`http --follow --verify=no $JOB_URL | jq -r '.results'`
+echo $RESULTS_URL
+[ $RESULTS_URL == "null" ] && exit 1
 
 echo "checking output and results..."
 http --headers --follow --verify=no HEAD $OUTPUT_URL | head -n 1 | fgrep "200 OK"
