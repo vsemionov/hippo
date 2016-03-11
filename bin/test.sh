57b582c9ca2fd534b87150a062c4ad8993d70542
Victor Semionov <vsemionov@gmail.com>
2016-03-11 06:14:28 +0200

    grep only the first line

--- bin/test.sh
+++ bin/test.sh
@@ -38,7 +38,7 @@ JOB_URL=`http -a admin:admin --verify=no --form POST https://$PROXY_IP/jobs/ inp
 [ $JOB_URL == "null" ] && exit 1
 
 echo "waiting for output and results..."
-http --headers --follow --verify=no HEAD $JOB_URL | fgrep "200 OK"
+http --headers --follow --verify=no HEAD $JOB_URL | head -n 1 | fgrep "200 OK"
 OUTPUT_URL=null
 while [ $OUTPUT_URL == "null" ]; do echo "waiting for output..."; sleep 1; OUTPUT_URL=`http --follow --verify=no $JOB_URL | jq -r '.output'`; done
 RESULTS_URL=null
